08ffdec94feaff2fa8fed0a8d695cab4369e6d9d
Steven Lam <stevenvl@gmail.com>
2020-02-20 04:48:47 -0500

    complted main card/running into error when attempting to pull from forecast api

--- js/script.js
+++ js/script.js
@@ -2,54 +2,40 @@ $("header").css({
     fontSize: 30
 });
 
-$("forecast").css({
+$("#forecast").css({
     fontSize:20
 });
-// var apiKEY = "f4591ef79b3933364d561e75edeb28da"
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +userInput+ "&appid="+ apiKEY;
 
-// $.ajax({
-// url: queryURL,
-// method: "GET"
-// });
+var todayPlusOne = moment().add(1, "days");
 
-var apiKey = "f4591ef79b3933364d561e75edeb28da";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-    "q=Bujumbura,Burundi&units=imperial&appid=" + apiKey;
+var cityName = "Philadelphia";
+var forecastURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + forecastAPIKEY;
+var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=" +cityName+ "&appid="+ apiKEY;
+var apiKEY = "f4591ef79b3933364d561e75edeb28da"
+var forecastAPIKEY ="7d11de971aee0dcc11aad0586e901657"
+
 
 $.ajax({
-    url: queryURL,
-    method: "GET"
+url: queryURL,
+method: "GET"
+})
+
+.then(function(response) {
+    console.log("MY api query", response);
+    $("#cityName").text(response.name + " " +moment().format("MMM Do, YYYY"));
+    $("#windSpeed").text("Wind Speed: " + response.wind.speed + "MPH");
+    $("#humidity").text("Humidity: " + response.main.humidity + "%");
+    var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+    $("#temp").text("Temperature" + " " + "°F");
+
 });
-// then(function(response) {
-//     console.log("MY api query", response);
-//     $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-//     $(".wind").text("Wind Speed: " + response.wind.speed);
-//     $(".humidity").text("Humidity: " + response.main.humidity);
-//     $(".temp").text("Temperature (F) " + response.main.temp);
-//     var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-//     $(".tempF").text("Temperature (Kelvin) " + tempF);
-// });
 
+$.ajax({
+url: forecastURL,
+method: "GET"
+})
+    
+.then(function(response) {
+console.log("forecast query",response)
+});
 
-
-/* <div class="list-group">
-  <a href="#" class="list-group-item list-group-item-action active">
-    Cras justo odio
-  </a>
-  <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
-  <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
-  <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-  <a href="#" class="list-group-item list-group-item-action disabled">Vestibulum at eros</a>
-</div>
-
-
-<div class="list-group">
-  <button type="button" class="list-group-item list-group-item-action active">
-    Cras justo odio
-  </button>
-  <button type="button" class="list-group-item list-group-item-action">Dapibus ac facilisis in</button>
-  <button type="button" class="list-group-item list-group-item-action">Morbi leo risus</button>
-  <button type="button" class="list-group-item list-group-item-action">Porta ac consectetur ac</button>
-  <button type="button" class="list-group-item list-group-item-action" disabled>Vestibulum at eros</button>
-</div> */
