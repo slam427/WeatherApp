a835ee2cd48f51026aa109f65cb9424b47a45da0
Steven Lam <stevenvl@gmail.com>
2020-02-20 03:21:13 -0500

    restructured grid/applied classes and ids to cards

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ $("header").css({
     fontSize: 30
 });
 
+$("forecast").css({
+    fontSize:20
+});
 // var apiKEY = "f4591ef79b3933364d561e75edeb28da"
 // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +userInput+ "&appid="+ apiKEY;
 
@@ -10,17 +13,22 @@ $("header").css({
 // method: "GET"
 // });
 
-var apiKey = "f4591ef79b3933364d561e75edeb28da"
+var apiKey = "f4591ef79b3933364d561e75edeb28da";
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-"q=Bujumbura,Burundi&units=imperial&appid=" + apiKey;
+    "q=Bujumbura,Burundi&units=imperial&appid=" + apiKey;
 
 $.ajax({
-url: queryURL,
-method: "GET"
+    url: queryURL,
+    method: "GET"
 });
-// .then(function(response) {
-console.log("MY api query",response);
-
+// then(function(response) {
+//     console.log("MY api query", response);
+//     $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+//     $(".wind").text("Wind Speed: " + response.wind.speed);
+//     $(".humidity").text("Humidity: " + response.main.humidity);
+//     $(".temp").text("Temperature (F) " + response.main.temp);
+//     var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+//     $(".tempF").text("Temperature (Kelvin) " + tempF);
 // });
 
 
