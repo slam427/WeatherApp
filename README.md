943491325cd38d27aef6563ce5466632d83bc999
Steven Lam <stevenvl@gmail.com>
2020-02-19 23:01:34 -0500

    skeleton of app created/api info also obtained

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # WeatherApp
-Displays current weather information for select cities
+The WeatherApp is a web based application that provides the user with location (city) specific weather information.
+
+This application provides detailed weather data for the specified city along with a 5-day forecast. 
+
+A history of the user's searches is also provided in a list in which the user can reference. 
+
---
+++ js/script.js
@@ -0,0 +1,35 @@
+$("nav").css({
+    fontSize: 30
+});
+
+// var apiKEY = "f4591ef79b3933364d561e75edeb28da"
+// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +userInput+ "&appid="+ apiKEY;
+
+// $.ajax({
+// url: queryURL,
+// method: "GET"
+// });
+
+
+
+
+// <div class="list-group">
+//   <a href="#" class="list-group-item list-group-item-action active">
+//     Cras justo odio
+//   </a>
+//   <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
+//   <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
+//   <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
+//   <a href="#" class="list-group-item list-group-item-action disabled">Vestibulum at eros</a>
+// </div>
+
+
+// <div class="list-group">
+//   <button type="button" class="list-group-item list-group-item-action active">
+//     Cras justo odio
+//   </button>
+//   <button type="button" class="list-group-item list-group-item-action">Dapibus ac facilisis in</button>
+//   <button type="button" class="list-group-item list-group-item-action">Morbi leo risus</button>
+//   <button type="button" class="list-group-item list-group-item-action">Porta ac consectetur ac</button>
+//   <button type="button" class="list-group-item list-group-item-action" disabled>Vestibulum at eros</button>
+// </div>
